e3ada166d4034ac40df3cac8339271bcb9e003c4
Alexander Kolli <alexander.kolli@uniklinikum.graz.at>
2024-10-25 14:35:31 +0200

    create example files

---
+++ examples/01_first_try.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'dbselftables/st_pathdef.inc.php';
+require_once $_stdbmariadb;
+require_once $_stsitecreator;
+
+$db= new STDbMariaDb();
+$db->connect('<host>', '<user>', '<password>');
+$db->database('<your preferred database>');
+
+$creator= new STSiteCreator($db);
+$creator->addCssLink('dbselftables/design/websitecolors.css');
+$creator->execute();
+$creator->display();
